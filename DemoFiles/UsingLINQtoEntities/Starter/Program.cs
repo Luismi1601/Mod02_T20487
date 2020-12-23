7ffd0a48be7779068781ea2a1aed9198e9de213c
Luismi1601 <Luismi16@gmail.com>
2020-12-23 16:37:42 +0100

    commit

---
+++ DemoFiles/UsingLINQtoEntities/Starter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using UsingLINQtoEntity.Database;
+using UsingLINQtoEntity.Models;
+using static System.Console;
+
+namespace UsingLINQtoEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           using (var context = new SchoolContext())
+            {
+                DbInitializer.Initialize(context);
+
+                var courses = from c in context.Courses select c;
+                
+                foreach (var course in courses)
+                {
+                    WriteLine($"Course: {course.Name}");
+                    foreach (var student in course.Students)
+                    {
+                        WriteLine($"\t Student name: {student.Name}");
+                    }
+                }
+                ReadLine();
+            } 
+        }
+    }
+}
